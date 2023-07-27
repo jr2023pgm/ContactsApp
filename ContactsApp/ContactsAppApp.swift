7ceb45da7a9071d9a8a8d5b4d3c0a711e3d340ea
Jay Ram <jr2023pgm@gmail.com>
2023-07-27 07:17:59 +0400

    Initial Commit

---
+++ ContactsApp/ContactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsAppApp.swift
+//  ContactsApp
+//
+//  Created by jr on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
